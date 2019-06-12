91b43612648f13151cab89d978972831220cce0d
Anita <anita.martin5703@gmail.com>
2019-06-12 16:29:27 -0600

    Added in data.

--- app/src/main/java/edu/cnm/deepdive/atthemovies/ActorsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/ActorsFragment.java
@@ -47,7 +47,7 @@ public class ActorsFragment extends Fragment {
 
     final Long movieId = ActorsFragmentArgs.fromBundle(getArguments()).getMovieId();
 
-    List<Actor> actors = viewModel.getMovie(movieId).getActors();
+    List<Actor> actors = viewModel.getMovie(movieId, context).getActors();
 
     final ArrayAdapter<Actor> adapter = new ArrayAdapter<Actor>(context, android.R.layout.simple_list_item_1, actors);
 
@@ -61,7 +61,7 @@ public class ActorsFragment extends Fragment {
       public void onClick(View v) {
         Actor newActor = new Actor();
         newActor.setName(newActorName.getText().toString());
-        viewModel.getMovie(movieId).getActors().add(newActor);
+        viewModel.getMovie(movieId, context).getActors().add(newActor);
         adapter.notifyDataSetChanged();
         newActorName.setText("");
       }
